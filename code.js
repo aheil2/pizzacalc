909cd420cc88127094e5c653bd51b76a12420fd3
aheil2 <71800212+aheil2@users.noreply.github.com>
2020-10-28 21:05:16 -0500

    jQuery update forgot to add "#" to IDs in js

--- code.js
+++ code.js
@@ -4,17 +4,17 @@ $(document).ready(function () {
 
     function pizzaCalc() {
         // Get amount of pizzas
-        var pizzas = $("pizzas").val();
+        var pizzas = $("#pizzas").val();
         // Parse to number
         pizzas = parseFloat(pizzas);
         // Get amount of toppings
-        var toppings = $("toppings").val();
+        var toppings = $("#toppings").val();
         // Parse to number
         toppings = parseFloat(toppings);
         // Get number of people
-        var people = $("persons").val();
+        var people = $("#persons").val();
         // Parse to number
-        people = parseInt(people);
+        people = parseFloat(people);
         // Calculate pizza cost
         var pizzaCost = pizzas * 15;
         // Calculate toppings cost
